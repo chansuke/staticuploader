a0bceaf131adf9ee3ff53af3c308027520aff263
chansuke <moonset20@gmail.com>
2015-12-15 01:32:28 +0900

    Update

--- Gemfile
+++ Gemfile
@@ -1,2 +1,3 @@
+gem 'padrino'
 gem 'sinatra'
 gem 'slim'
--- app.rb
+++ app.rb
@@ -1,1 +1,31 @@
 require 'sinatra'
+
+get '/index' do
+  slim :index
+end
+
+post '/uploader' do
+  if params[:file]
+    save_path = "./public/images/#{params[:file][:filename]}"
+
+    File.open(save_path, 'wb') do |f|
+      p params[:file][:template]
+      f.write params[:file][:template].read
+      @mes = "success"
+    end
+  else
+    @mes = "fail"
+  end
+  slim :uploader
+  redirect 'images'
+end
+
+get '/images' do
+  images = Dir.glob("public/images/*")
+  @images_path = []
+
+  images.each do |image|
+    @images_path << image.gsub("public/", "./")
+  end
+  slim :images
+end
